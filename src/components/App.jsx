ab92e3b2e3ec1e5643ba75885d347e27b8cfa323
Danik2023 <danik.kyrteu2@gmail.com>
2025-01-27 13:44:45 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,39 @@
-import { Component } from 'react';
 import Notiflix from 'notiflix';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactListComponent } from './ContactList/ContactList';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  componentDidMount() {
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem('contact');
     if (savedContacts !== null) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
+      return JSON.parse(savedContacts);
     } else {
-      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+      localStorage.setItem(
+        'contact',
+        JSON.stringify([
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ])
+      );
     }
-  }
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contact', JSON.stringify(contacts));
+  }, [contacts]);
 
-  addNumber = ({ name, number }) => {
-    const contactWithSameName = this.state.contacts.find(
+  const addNumber = ({ name, number }) => {
+    const contactWithSameName = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
 
-    const contactWithSameNumber = this.state.contacts.find(
+    const contactWithSameNumber = contacts.find(
       contact => contact.number === number
     );
 
@@ -50,40 +47,33 @@ export class App extends Component {
       Notiflix.Notify.failure(`Контакт з номером ${number} вже існує!`);
     } else {
       const newContact = { id: nanoid(), name, number };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      setContacts(prevState => [...prevState, newContact]);
       Notiflix.Notify.success(`Контакт ${name} успішно додано!`);
     }
   };
 
-  filterContacts = event => {
-    this.setState({ filter: event.target.value });
+  const filterContacts = event => {
+    setFilter(event.target.value);
   };
 
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleDelete = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(
-      contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-        contact.number.includes(filter)
-    );
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+  );
 
-    return (
-      <div style={{ marginBottom: '50px' }}>
-        <ContactForm onSubmit={this.addNumber} />
-        <Filter value={filter} onChange={this.filterContacts} />
-        <ContactListComponent
-          contacts={filteredContacts}
-          onDelete={this.handleDelete}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div style={{ marginBottom: '50px' }}>
+      <ContactForm onSubmit={addNumber} />
+      <Filter value={filter} onChange={filterContacts} />
+      <ContactListComponent
+        contacts={filteredContacts}
+        onDelete={handleDelete}
+      />
+    </div>
+  );
+};
